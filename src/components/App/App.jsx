60d92912e769e93f68ded9bb294036a04acdfdf9
Taisiya Tomashevska <taya22011985@gmail.com>
2023-03-22 16:25:38 +0200

    add hook localestorage

---
+++ src/components/App/App.jsx
@@ -0,0 +1,44 @@
+import { Section } from '../Section/Section';
+import { Form } from '../Form/Form';
+import { ContactList } from '../ContactList/ContactList';
+import { Filter } from '../Filter/Filter';
+import { useLocaleStorage } from 'hooks/useLocaleStorage';
+import css from './App.module.css';
+
+export const App = () => {
+  const [contacts, setContacts] = useLocaleStorage('contacts', []);
+  const [filter, setFilter] = useLocaleStorage('filter', '');
+
+  const formSubmitHandler = newContact => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const onDelete = idContact => {
+    setContacts(contacts.filter(contact => contact.id !== idContact));
+  };
+
+  const onChangeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const getFilterContact = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  const filterContact = getFilterContact();
+  return (
+    <div className={css.wrapper}>
+      <Section title="Phone book">
+        <Form onSubmit={formSubmitHandler} contacts={contacts} />
+      </Section>
+      {contacts.length > 0 && (
+        <>
+          <Section title="Contacts" />
+          <Filter value={filter} onChange={onChangeFilter} />
+          <ContactList contacts={filterContact} onDelete={onDelete} />
+        </>
+      )}
+    </div>
+  );
+};
